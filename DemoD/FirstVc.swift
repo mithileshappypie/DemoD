bc77be99503a01e02c9c88c62717f8789969a66a
Mithilesh <mithilesh@appypiellp.com>
2019-11-13 18:19:56 +0530

    First view controller added

---
+++ DemoD/FirstVc.swift
@@ -0,0 +1,30 @@
+//
+//  FirstVc.swift
+//  DemoD
+//
+//  Created by Mithilesh on 13/11/19.
+//  Copyright © 2019 Mithilesh. All rights reserved.
+//
+
+import UIKit
+
+class FirstVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
